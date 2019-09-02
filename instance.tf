2b19c16f6ec4d201c1baa1e265ae62a03c013513
Connor <connor.aitken@gmail.com>
2019-09-02 16:33:35 -0400

    updating ebs volume settings

--- instance.tf
+++ instance.tf
@@ -6,8 +6,28 @@ resource "aws_instance" "unifi" {
     key_name = "${var.ssh_key_pair_name}"
     subnet_id = "${aws_subnet.subnet-1a.id}"
     user_data = "${file("user_data.sh")}"
+    root_block_device {
+        volume_type = "gp2"
+        volume_size = "60"
+        delete_on_termination = "false"
+    }
 
     tags {
         Name = "unifi"
     }
 }
+
+// resource "aws_ebs_volume" "unifi-volume" {
+//   availability_zone = "us-east-1a"
+//   size              = 60
+
+//   tags {
+//     Name = "unifi"
+//   }
+// }
+
+// resource "aws_volume_attachment" "unifi-volume-attachment" {
+//   device_name = "/dev/sda2"
+//   instance_id = "${aws_instance.unifi.id}"
+//   volume_id   = "${aws_ebs_volume.unifi-volume.id}"
+// }
